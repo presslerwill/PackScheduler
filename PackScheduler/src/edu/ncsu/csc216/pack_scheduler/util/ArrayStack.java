5432759d4d1d13db7748f8cee456bdd9b5dc951b
Will Pressler <wdpressl@ncsu.edu>
2022-03-28 18:57:55 -0400

    [Testing] Fully tested and implemented ArrayStack

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayStack.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayStack.java
@@ -3,10 +3,90 @@
  */
 package edu.ncsu.csc216.pack_scheduler.util;
 
+import java.util.EmptyStackException;
+
 /**
- * @author press
+ * Stack list with an Array based implementation
+ * @author Will Pressler
+ * @param <E>
  *
  */
-public class ArrayStack implements Stack<E> {
+public class ArrayStack<E> implements Stack<E> {
+	
+	/** ArrayList to which ArrayStack delegates functionality */
+	private ArrayList<E> stack;
+	/** The capacity of the stack */
+	private int capacity;
+
+	/**
+	 * Constructor for the ArrayStack
+	 * @param capacity the capacity of the ArrayStack
+	 */
+	public ArrayStack(int capacity) {
+		stack = new ArrayList<E>();
+		setCapacity(capacity);
+	}
+	
+	/**
+	 * Pushes an element on to the top of the stack
+	 * @param element the element to be added to the stack
+	 */
+	@Override
+	public void push(E element) {
+		if (capacity == stack.size()) {
+			throw new IllegalArgumentException("No more room");
+		}
+		stack.add(element);
+		
+	}
+
+	/**
+	 * Pops the top element off of the stack
+	 * @return E the element that was removed from the stack
+	 */
+	@Override
+	public E pop() {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		return stack.remove(size() - 1);
+	}
+
+	/**
+	 * Checks if the stack is empty
+	 * @return boolean true if the stack has 0 elements, false otherwise
+	 */
+	@Override
+	public boolean isEmpty() {
+		boolean ret = false;
+		if(stack.size() == 0) {
+			ret = true;
+		}
+		return ret;
+	
+	}
+
+	/**
+	 * Gets the size of the Stack
+	 * @return int the number of elements in the stack
+	 */
+	@Override
+	public int size() {
+		return stack.size();
+	}
+
+	/**
+	 * Sets the capacity of the the Stack
+	 * @param capacity the desired capacity of the Stack
+	 * @throws IllegalArgumentException if capacity is less than zero or the current size
+	 */
+	@Override
+	public void setCapacity(int capacity) {
+		if (capacity < 0 || capacity < stack.size()) {
+			throw new IllegalArgumentException("Invalid capacity");
+		}
+		this.capacity = capacity;
+		
+	}
 
 }
--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayStackTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayStackTest.java
@@ -3,10 +3,118 @@
  */
 package edu.ncsu.csc216.pack_scheduler.util;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.EmptyStackException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
- * @author press
+ * Tests the ArrayStack class
+ * @author Will Pressler
  *
  */
 public class ArrayStackTest {
 
+	/** Stack to be used in tests */
+	private ArrayStack<String> stack;
+	/**
+	 * Sets up an array with capacity 5 before the tests
+	 */
+	@BeforeEach
+	public void setUp() {
+		stack = new ArrayStack<String>(5);
+		
+	}
+	/**
+	 * Tests ArrayStack's push method
+	 */
+	@Test
+	public void testPush() {
+		assertTrue(stack.isEmpty());
+		stack.push("String1");
+		assertEquals(1, stack.size());
+		assertFalse(stack.isEmpty());
+		stack.push("String2");
+		stack.push("String3");
+		assertEquals(3, stack.size());
+		stack.push("String4");
+		stack.push("String5");
+		assertThrows(IllegalArgumentException.class, () -> stack.push("String6"));
+		assertEquals(5, stack.size());
+		assertEquals("String5", stack.pop());
+		assertEquals(4, stack.size());
+		stack.push("String6");
+		assertEquals(5, stack.size());
+	}
+	
+	/**
+	 * Tests ArrayStack's pop method
+	 */
+	@Test
+	public void testPop() {
+		assertTrue(stack.isEmpty());
+		assertThrows(EmptyStackException.class, () -> stack.pop());
+		stack.push("String1");
+		stack.push("String2");
+		stack.push("String3");
+		assertEquals(3, stack.size());
+		assertEquals("String3", stack.pop());
+		assertEquals(2, stack.size());
+		assertEquals("String2", stack.pop());
+		assertEquals("String1", stack.pop());
+		assertThrows(EmptyStackException.class, () -> stack.pop());
+	}
+	
+	/**
+	 * Tests ArrayStack's isEmpty method
+	 */
+	@Test
+	public void testIsEmpty() {
+		assertTrue(stack.isEmpty());
+		stack.push("String1");
+		assertFalse(stack.isEmpty());
+	}
+	
+	/**
+	 * Tests ArrayStack's size method
+	 */
+	@Test
+	public void testSize() {
+		assertEquals(0, stack.size());
+		stack.push("String1");
+		assertEquals(1, stack.size());
+		stack.push("String2");
+		assertEquals(2, stack.size());
+		stack.push("String3");
+		assertEquals(3, stack.size());
+		stack.push("String4");
+		assertEquals(4, stack.size());
+		stack.push("String5");
+		assertEquals(5, stack.size());
+		
+		assertThrows(IllegalArgumentException.class, () -> stack.push("String6"));
+		assertEquals(5, stack.size());
+	}
+	
+	/**
+	 * Tests ArrayStack's setCapacity method
+	 */
+	@Test
+	public void testSetCapacity() {
+		stack.push("String1");
+		assertThrows(IllegalArgumentException.class, () -> stack.setCapacity(-2));
+		stack.push("String2");
+		stack.push("String3");
+		assertThrows(IllegalArgumentException.class, () -> stack.setCapacity(2));
+		stack.push("String4");
+		stack.push("String5");
+		assertThrows(IllegalArgumentException.class, () -> stack.push("String6"));
+		stack.setCapacity(7);
+		stack.push("String6");
+		stack.push("String7");
+		assertEquals(7, stack.size());
+		assertThrows(IllegalArgumentException.class, () -> stack.push("String"));
+	}
 }
