0483fce72894f6660f561ae68cb316241d6fdf79
Jason Wong <jtwong2@ncsu.edu>
2022-03-28 18:41:37 -0400

    [Implementation,Test]Implemented LinkedStack with associated unit tests

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedStack.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedStack.java
@@ -1,12 +1,76 @@
-/**
- * 
- */
 package edu.ncsu.csc216.pack_scheduler.util;
 
 /**
- * @author press
+ * This class implements a linked list-based stack. This stack has
+ * methods to push, pop, get the size, get if the stack is currently
+ * empty, and set capacity. In a stack, only the top (most recently
+ * added) element can be accessed.
+ * 
+ * @author Jason Wong
+ * @param <E> generic type used in this LinkedStack
  *
  */
-public class LinkedStack implements Stack<E> {
+public class LinkedStack<E> implements Stack<E> {
+    /** Underlying linked list that this LinkedStack delegates to */
+    private LinkedAbstractList<E> stack;
+   
+    /**
+     * Constructs a new LinkedStack with the given capacity
+     * 
+     * @param capacity capacity to set to
+     */
+    public LinkedStack(int capacity) {
+        stack = new LinkedAbstractList<E>(capacity);
+    }
+   
+    /**
+     * Implements Stack's push() to add the given element to the top of the stack.
+     * 
+     * @param element element to add to top of stack
+     * @throws IllegalArgumentException if stack is full
+     */
+    @Override
+    public void push(E element) {
+        stack.add(0, element);
+    }
 
+    /**
+     * Implements Stack's pop() method to remove and return the element at the top of the stack
+     * 
+     * @return the element that was removed
+     */
+    @Override
+    public E pop() {
+        return stack.remove(0);
+    }
+
+    /**
+     * Implements Stack's isEmpty() method to check if the stack is empty
+     * @return true if stack contains no elements
+     */
+    @Override
+    public boolean isEmpty() {
+        return stack.size() == 0;
+    }
+
+    /**
+     * Implements Stack's size() method to get the current size of this LinkedStack
+     * @return size of stack
+     */
+    @Override
+    public int size() {
+        return stack.size();
+    }
+
+    /**
+     * Implements Stack's setCapacity() method to set the capacity of this LinkedStack
+     * 
+     * @param capacity capacity to set
+	 * @throws IllegalArgumentException if the parameter is negative or less than the
+	 *                                  number of elements in the stack
+     */
+    @Override
+    public void setCapacity(int capacity) {
+        stack.setCapacity(capacity);
+    }
 }
--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedStackTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedStackTest.java
@@ -1,12 +1,94 @@
-/**
- * 
- */
 package edu.ncsu.csc216.pack_scheduler.util;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
- * @author press
+ * Tests methods from the LinkedStack class
+ * 
+ * @author Jason Wong
  *
  */
-public class LinkedStackTest {
+class LinkedStackTest {
+    /** LinkedStack object for use in tests */
+    LinkedStack<String> stack;
+    /** Default capacity for the LinkedStack objects in tests */
+    private static final int CAPACITY = 2;
+   
+    /**
+     * Constructs the LinkedStack object with the default capacity before each test
+     */
+    @BeforeEach
+    void setup() {
+        stack = new LinkedStack<String>(CAPACITY);
+    }
+
+    /**
+     * Test method for the LinkedStack constructor.
+     */
+    @Test
+    void testLinkedStack() {
+        stack.push("1");
+        stack.push("2");
+        assertThrows(IllegalArgumentException.class, () -> stack.push("3"));
+    }
+
+    /**
+     * Test method for push() and pop().
+     */
+    @Test
+    void testPushAndPop() {
+        stack.push("1");
+        stack.push("2");
+        
+        assertEquals(2, stack.size());
+        
+        assertEquals("2", stack.pop());
+        assertEquals("1", stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+
+    /**
+     * Test method for isEmpty().
+     */
+    @Test
+    void testIsEmpty() {
+        assertTrue(stack.isEmpty());
+        stack.push("1");
+        assertFalse(stack.isEmpty());
+        stack.push("2");
+        assertFalse(stack.isEmpty());
+    }
+
+    /**
+     * Test method for size().
+     */
+    @Test
+    void testSize() {
+        assertEquals(0, stack.size());
+        stack.push("1");
+        assertEquals(1, stack.size());
+        stack.push("2");
+        assertEquals(2, stack.size());
+    }
+
+    /**
+     * Test method for setCapcity().
+     */
+    @Test
+    void testSetCapacity() {
+        assertThrows(IllegalArgumentException.class, () -> stack.setCapacity(-1));
+        stack.push("1");
+        stack.push("2");
+        assertThrows(IllegalArgumentException.class, () -> stack.push("3"));
+        stack.setCapacity(4);
+        stack.push("3");
+        stack.push("4");
+        assertThrows(IllegalArgumentException.class, () -> stack.push("5"));
+        
+        assertThrows(IllegalArgumentException.class, () -> stack.setCapacity(2));
+    }
 
 }
