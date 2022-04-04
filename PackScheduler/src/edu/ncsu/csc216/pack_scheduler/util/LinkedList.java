55b9cf6d6ebc09d75f1dcb45967b7c43b94c90e1
hsoconne <hsoconne@ncsu.edu>
2022-04-04 18:47:57 -0400

    [Implementation][Testing] update the LinkedList class and its inner classes and begin the associated tests

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedList.java
@@ -0,0 +1,197 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import java.util.AbstractSequentialList;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 
+ * @author Helen O'Connell
+ * @author Jason Wong
+ * @author Will Pressler
+ *
+ * @param <E>
+ */
+public class LinkedList<E> extends AbstractSequentialList<E> {
+
+	/** first ListNode of the list */
+	private ListNode front;
+	/** last ListNode of the list */
+	private ListNode back;
+	/** number of elements in the list */
+	private int size;
+
+	/**
+	 * Constructor for a LinkedList. creates two ListNodes that have null data to
+	 * act as dummy nodes
+	 */
+	public LinkedList() {
+		this.front = new ListNode(null);
+		this.back = new ListNode(null);
+		this.size = 0;
+		front.next = back;
+		back.prev = front;
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		return new LinkedListIterator(index);
+	}
+
+	@Override
+	public void add(int index, E element) {
+		if (contains(element)) {
+			throw new IllegalArgumentException();
+		}
+		ListIterator<E> toAddIterator = new LinkedListIterator(index);
+		toAddIterator.add(element);
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	private class ListNode {
+		/** the data contained in the node */
+		private E data;
+		/** the node following this node */
+		private ListNode next;
+		/** the node before this node */
+		private ListNode prev;
+
+		/**
+		 * Constructor for a ListNode without a next and prev value specified. delegates
+		 * to the other constructor for ListNode
+		 * 
+		 * @param data of the ListNode being created
+		 */
+		public ListNode(E data) {
+			this(data, null, null);
+		}
+
+		/**
+		 * Constructor for a ListNode with a prev and next value specified. directly
+		 * sets the data, prev, and next fields
+		 * 
+		 * @param data of the ListNode being created
+		 * @param prev of the ListNode being created
+		 * @param next of the ListNode being created
+		 */
+		public ListNode(E data, ListNode prev, ListNode next) {
+			this.data = data;
+			this.prev = prev;
+			this.next = next;
+		}
+	}
+
+	private class LinkedListIterator implements ListIterator<E> {
+
+		/** represents the returned ListNode on a call to the previous method */
+		private ListNode previous;
+		/** represents the returned ListNode on a call to the next method */
+		private ListNode next;
+		/** the index returned by a call to the previous index method */
+		private int previousIndex;
+		/** the index returned by a call to the next index method */
+		private int nextIndex;
+		/**
+		 * the most recently returned ListNode by a call from the previous or next
+		 * method
+		 */
+		private ListNode lastRetrieved;
+
+		/**
+		 * Iterates through the list using a current value to position the iterator
+		 * between the element before the one at the index given and the one at the
+		 * index given. then sets the appropriate fields
+		 * 
+		 * @param index of where to position the iterator
+		 */
+		public LinkedListIterator(int index) {
+			if (index < 0 || index > size) {
+				throw new IndexOutOfBoundsException();
+			}
+			ListNode current = front;
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+			previous = current;
+			next = current.next;
+			previousIndex = index - 1;
+			nextIndex = index;
+			lastRetrieved = null;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return (next.data != null);
+		}
+
+		@Override
+		public E next() {
+			if (next.data == null) {
+				throw new NoSuchElementException();
+			}
+			next = next.next;
+			lastRetrieved = next.prev;
+			nextIndex = nextIndex + 1;
+			previousIndex = previousIndex + 1;
+			return lastRetrieved.data;
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			return (previous.data != null);
+		}
+
+		@Override
+		public E previous() {
+			if (previous.data == null) {
+				throw new NoSuchElementException();
+			}
+			previous = previous.prev;
+			lastRetrieved = previous.next;
+			nextIndex = nextIndex - 1;
+			previousIndex = previousIndex - 1;
+			return lastRetrieved.data;
+		}
+
+		@Override
+		public int nextIndex() {
+			return nextIndex;
+		}
+
+		@Override
+		public int previousIndex() {
+			return previousIndex;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void set(E e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void add(E e) {
+			if (e == null) {
+				throw new NullPointerException();
+			}
+			ListNode toAdd = new ListNode(e, next, previous);
+			previous.next = toAdd;
+			next.prev = toAdd;
+			previous = toAdd;
+			previousIndex++;
+			nextIndex++;
+			size++;
+			lastRetrieved = null;
+		}
+	}
+}
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedListTest.java
@@ -0,0 +1,129 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.ncsu.csc216.pack_scheduler.course.Course;
+
+class LinkedListTest {
+
+	/**
+	 * Test method for
+	 * {@link edu.ncsu.csc216.pack_scheduler.util.ArrayList#ArrayList()}.
+	 */
+	@Test
+	void testConstructor() {
+		LinkedList<String> stringList = new LinkedList<String>();
+		assertEquals(0, stringList.size());
+
+		LinkedList<Course> courseList = new LinkedList<Course>();
+		assertEquals(0, courseList.size());
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.util.ArrayList#size()}.
+	 */
+	@Test
+	void testSize() {
+		LinkedList<String> array = new LinkedList<String>();
+		array.add("string1");
+		assertEquals(1, array.size());
+
+		array.add("string2");
+		assertEquals(2, array.size());
+
+		array.remove(1);
+		assertEquals(1, array.size());
+
+		array.remove(0);
+		assertEquals(0, array.size());
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractList#add(int, java.lang.Object)}.
+	 */
+	@Test
+	void testAddIntE() {
+		LinkedList<String> array = new LinkedList<String>();
+		assertThrows(NullPointerException.class, () -> array.add(0, null));
+
+		array.add(0, "String");
+
+		array.add(1, "String");
+		assertThrows(IllegalArgumentException.class, () -> array.add(1, "String"));
+
+		array.add(1, "String2");
+
+		array.add(1, "String3");
+
+		assertThrows(IndexOutOfBoundsException.class, () -> array.add(-1, "str"));
+		assertThrows(IndexOutOfBoundsException.class, () -> array.add(4, "str"));
+		
+		array.add("4");
+		array.add("5");
+		array.add("6");
+		array.add("7");
+		array.add("8");
+		array.add("9");
+		array.add("10");
+		array.add("11");
+		assertEquals(11, array.size());
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractList#set(int, java.lang.Object)}.
+	 */
+	@Test
+	void testSet() {
+		LinkedList<String> array = new LinkedList<String>();
+		array.add("String1");
+		array.add("String2");
+		array.add("String3");
+
+		array.set(1, "New String");
+		
+		array.set(0, "New String2");
+		
+		array.set(2, "New String3");
+		
+		assertThrows(IndexOutOfBoundsException.class, () -> array.set(-1, "string")); 
+		assertThrows(IndexOutOfBoundsException.class, () -> array.set(3, "string"));
+		
+		assertThrows(NullPointerException.class, () -> array.set(0, null)); 
+
+
+
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractList#remove(int)}.
+	 */
+	@Test
+	void testRemoveInt() {
+		LinkedList<String> array = new LinkedList<String>();
+		array.add("String1");
+		array.add("String2");
+		array.add("String3");
+		
+		String ret = array.remove(2);
+		assertEquals("String3", ret);
+		assertEquals(2, array.size()); 
+		
+		ret = array.remove(0); 
+		assertEquals("String1", ret);
+		assertEquals(1, array.size());
+		
+		array.add("str1");
+		array.add("str2");
+		
+		ret = array.remove(1); 
+		assertEquals("str1", ret);
+		assertEquals(2, array.size());
+		
+		
+		assertThrows(IndexOutOfBoundsException.class, () -> array.remove(-1)); 
+		assertThrows(IndexOutOfBoundsException.class, () -> array.remove(3));
+			}
+
+}
